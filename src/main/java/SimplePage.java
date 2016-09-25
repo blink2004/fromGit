998193e7b44307d8593741621c6ac522141eb2e5
Freeman <blink2004@mail.ru>
2016-09-26 01:28:38 +0300

    Init

---
+++ src/main/java/SimplePage.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Freeman on 26.09.2016.
+ */
+public class SimplePage {
+
+    private WebDriver driver;
+
+    public void clickButton(String name) {
+    }
+
+}
---
+++ src/test/java/TestSample.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Freeman on 26.09.2016.
+ */
+public class TestSample {
+
+    public WebDriver driver;
+
+    protected String domain = "http://ya.ru";
+
+    @BeforeSuite
+    public void before() {
+//        String webdriversPath = "D:\\Program Files (x86)\\webdrivers\\";
+        String webdriversPath = "./";
+
+        System.setProperty("webdriver.chrome.bin",
+                webdriversPath + "chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(domain);
+    }
+
+    @org.testng.annotations.Test
+    public void testGit() {
+        System.out.println("Simple test from git");
+    }
+
+    @AfterSuite
+    public void after() {
+        driver.quit();
+    }
+
+}
