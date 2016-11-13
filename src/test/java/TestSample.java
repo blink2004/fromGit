78ff17bd0b90c9742bce44b179cf64b6038c230a
Mr. Freeman <blink2004@mail.ru>
2016-11-14 00:19:54 +0200

    Fix path to chrome webdriver. Attempt 8.

--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -28,7 +28,7 @@ public class TestSample {
 
         // for Linux
 //        System.setProperty("webdriver.chrome.bin",
-        System.setProperty("webdriver.chrome.driver", "../linux_chromedriver");
+        System.setProperty("webdriver.chrome.driver", "./../linux_chromedriver");
 
         driver = new ChromeDriver();
 //        driver = new FirefoxDriver();
